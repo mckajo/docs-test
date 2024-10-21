0c23719c8b5829e2d1b6cc8abe6e4944b9ffce47
AJ <ahmed_hussein_al_obaidi@external.mckinsey.com>
2024-10-21 18:15:23 +0400

    publish to pages

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -21,7 +21,7 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
+  title: 'Docs Test', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://your-docusaurus-test-site.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
+  projectName: 'menshen',
   organizationName: 'facebook',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
